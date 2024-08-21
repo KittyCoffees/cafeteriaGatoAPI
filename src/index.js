498c587d631a18dad997b0b6a410f8576f593aeb
Priscila <pyea08@gmail.com>
2024-08-21 19:07:17 -0300

    1.0

--- src/index.js
+++ src/index.js
@@ -6,13 +6,21 @@ const comidaController= require("./modules/comida");
 const app = express();
 app.use(express.json());
 
-app.post("/bebida", (req, res) => {
+app.get('/', (req,res)=> res.status(200).send(`
+Bem vindos ao Kittycoffes!!
+Gostaria de fazer um pedido e visitar nossos gatinhos?
+`))
+app.post("/bebida", (req, res) =>{
     const bebida = req.body;
     const code = bebidaController.create(bebida);
     res.status(code).json();
   });
   
   app.get("/bebida", (req, res) => {
+    res.status(200).send(`
+    "nome":
+    "preco":
+    "tipo": (quente ou gelada)`)
     const bebidas = bebidaController.index();
     res.json(bebidas);
   });
@@ -41,6 +49,10 @@ app.post("/bebida", (req, res) => {
   });
   
   app.get("/comida", (req, res) => {
+    res.status(200).send(`
+    "nome":
+    "preco":
+    "tipo": (doce ou salgada)`)
     const comidas = comidaController.index();
     res.json(comidas);
   });
@@ -61,4 +73,66 @@ app.post("/bebida", (req, res) => {
       res.json()
   })
 
-app.listen(3000, ()=> console.log("server rodando.."));    
+  //cliente 
+
+  app.post("/cliente", (req, res) => {
+    const cliente = req.body;
+    const code = clienteController.create(cliente);
+    res.status(code).json();
+  });
+  
+  app.get("/cliente", (req, res) => {
+    res.status(200).send(`
+    "nome":`)
+    const clientes = clienteController.index();
+    res.json(clientes);
+  });
+  
+  app.get("/cliente/:id", (req, res) => {
+    const cliente = clienteController.show(req.params.id);
+    res.json(cliente);
+  });
+  
+  app.put("/cliente/:id", (req, res) => {
+      const cliente = req.body
+      const code = clienteController.update(req.params.id, cliente)
+      res.status(code).json()
+  })
+  
+  app.delete("/cliente/:id", (req, res) => {
+      clienteController.destroy(req.params.id)
+      res.json()
+  })
+
+  //funcionario
+
+  app.post("/funcionario", (req, res) => {
+    const funcionario = req.body;
+    const code = funcionarioController.create(funcionario);
+    res.status(code).json();
+  });
+  
+  app.get("/funcionario", (req, res) => {
+    res.status(200).send(`
+    "nome":`)
+    const funcionarios = funcionarioController.index();
+    res.json(funcionarios);
+  });
+  
+  app.get("/funcionario/:id", (req, res) => {
+    const funcionario = funcionarioController.show(req.params.id);
+    res.json(funcionario);
+  });
+  
+  app.put("/funcionario/:id", (req, res) => {
+      const funcionario = req.body
+      const code = funcionarioController.update(req.params.id, funcionario)
+      res.status(code).json()
+  })
+  
+  app.delete("/funcionario/:id", (req, res) => {
+      funcionarioController.destroy(req.params.id)
+      res.json()
+  })
+
+app.listen(4000, ()=> console.log("server rodando.."));    
--- src/rotas.js
+++ src/rotas.js
@@ -3,3 +3,5 @@ const express = require("express");
 const router = express.Router();
 router.get('/', (req,res)=> res.status(200).send("As rotas estão funcionando!"))
 module.exports = router
+
+
